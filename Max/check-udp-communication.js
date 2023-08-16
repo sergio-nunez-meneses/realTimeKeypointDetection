5f7900467c407d8c429a2035dc107312668ac46e
sergionunezmeneses <snunezmeneses@gmail.com>
2023-08-16 22:06:36 +0200

    WIP add functions checkMessageFormat and printErrors (check-udp-communication.js)

--- Max/check-udp-communication.js
+++ Max/check-udp-communication.js
@@ -3,56 +3,78 @@
 // ============================================================================
 autowatch = 1;
 inlets    = 1;
-outlets   = 1;
+outlets   = 2;
+
+// ============================================================================
+//  Variables
+// ============================================================================
+var addresses = ["connect"];
 
 // ============================================================================
 //  Functions
 // ============================================================================
 function anything() {
+	var errors = checkMessageFormat(messagename.slice(1), arguments);
+
+	if (errors.length > 0) {
+		printErrors(errors);
+	}
+	else {
+		/*
+		var response = {};
+		var parse;
+		else {
+			parse = JSON.parse(message);
+
+			if (typeof parse["connected"] !== "boolean") {
+				errors.push("OSC parsed value must be of type boolean");
+			}
+		}
+
+		if (errors.length > 0) {
+			printErrors(errors);
+
+			response["errors"] = errors.join(", ");
+		}
+		else {
+			response["connected"] = !parse["connected"] ? true : parse["connected"];
+		}
+		outlet(0, ["/connect", JSON.stringify(response)]);
+		*/
+	}
+}
+
+function checkMessageFormat(address, data) {
 	var errors = [];
 
-	var address = messagename;
-	if (address.slice(1) !== "connect") {
-		errors.push("OSC address pattern must be /connect");
+	if (addresses.indexOf(address) === -1) {
+		errors.push("OSC address pattern must be /connect, /left_hand, or /right_hand");
 	}
 
-	if (arguments.length === 0) {
+	if (data.length === 0) {
 		errors.push("OSC argument must not be empty");
 	}
-	if (arguments.length > 1) {
+	if (data.length > 1) {
 		errors.push("OSC argument must not have more than 1 element");
 	}
 
-	var message = arguments[0];
+	var message = data[0];
 	if (typeof message !== "string") {
 		errors.push("OSC argument must be of type string");
 	}
 
 	var re = new RegExp(/\{([^}]+)\}/);
 	var params = re.exec(message);
-	var response = {};
-	var parse;
 
 	if (params === null) {
 		errors.push("OSC parsed argument must be of type JSON string");
 	}
-	else {
-		parse = JSON.parse(message);
 
-		if (typeof parse["connected"] !== "boolean") {
-			errors.push("OSC parsed value must be of type boolean");
-		}
-	}
+	return errors;
+}
 
-	if (errors.length > 0) {
-		for (var i = 0; i < errors.length; i++) {
-			post("Error: " + errors[i] + "\n");
-		}
-		response["errors"] = errors.join(", ");
+function printErrors(errors) {
+	for (var i = 0; i < errors.length; i++) {
+		post("Error: " + errors[i] + "\n");
 	}
-	else {
-		response["connected"] = !parse["connected"] ? true : parse["connected"];
-	}
-
-	outlet(0, ["/connect", JSON.stringify(response)]);
 }
