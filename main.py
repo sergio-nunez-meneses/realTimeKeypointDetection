f909250c2c372c57f36e1c2e07a60c7b21735148
sergionunezmeneses <snunezmeneses@gmail.com>
2023-09-13 10:35:54 +0200

    Add class HandLandmarksHandler & Refactor main execution (main.py)

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ class UDPCommunicationHandler:
 			response = json.loads(message)
 
 			if "errors" in response:
-				self.print_errors(response["errors"].split(", "))
+				print_errors(response["errors"].split(", "))
 
 			if not isinstance(response["connected"], bool):
 				errors.append("OSC parsed value must be of type boolean")
@@ -118,12 +118,85 @@ class UDPCommunicationHandler:
 				errors.append("Error while communicating with UDP client through port {}".format(self.client_port))
 
 		if len(errors) > 0:
-			self.print_errors(errors)
+			print_errors(errors)
 
-	def print_errors(self, errors):
-		for i in range(len(errors)):
-			print(errors[i])
-		exit(1)
+
+class HandLandmarksHandler:
+	def __init__(self, mp_solutions, min_detection_confidence, min_tracking_confidence):
+		self.named_hand_landmarks = [
+			"wrist", "thumb_cmc", "thumb_mcp", "thumb_ip", "thumb_tip", "index_finger_mcp", "index_finger_pip",
+			"index_finger_dip", "index_finger_tip", "middle_finger_mcp", "middle_finger_pip", "middle_finger_dip",
+			"middle_finger_tip", "ring_finger_mcp", "ring_finger_pip", "ring_finger_dip", "ring_finger_tip",
+			"pinky_mcp", "pinky_pip", "pinky_dip", "pinky_tip"
+		]
+		self.landmarks_to_render = ["wrist", "thumb_tip", "index_finger_tip", "middle_finger_tip", "ring_finger_tip",
+		                            "pinky_tip"]
+		self.landmarks = {
+			"left_hand":  None,
+			"right_hand": None
+		}
+
+		self.holistic = mp_solutions.holistic
+		self.inference = self.holistic.Holistic(min_detection_confidence=min_detection_confidence,
+		                                        min_tracking_confidence=min_tracking_confidence)
+		self.drawing = mp_solutions.drawing_utils
+		self.drawing_styles = mp_solutions.drawing_styles
+
+		self.image = None
+		self.results = None
+		self.conn = None
+
+	def run_inference(self, frame):
+		self.image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+		self.image.flags.writeable = False
+
+		self.results = self.inference.process(self.image)
+
+		self.image.flags.writeable = True
+		self.image = cv.cvtColor(self.image, cv.COLOR_RGB2BGR)
+
+	# TODO: Send data as list
+	def process_inference_data(self, udp, hand_names):
+		# TODO: Refactor dict initialization
+		self.landmarks["left_hand"] = self.results.left_hand_landmarks
+		self.landmarks["right_hand"] = self.results.right_hand_landmarks
+
+		for x in range(len(hand_names)):
+			hand_name = hand_names[x]
+			address = "/{}".format(hand_name)
+
+			# Get coordinates from landmarks
+			if self.landmarks[hand_name] is not None:
+				udp.send(address, {"isVisible": True})
+
+				for y in range(len(self.named_hand_landmarks)):
+					landmark_name = self.named_hand_landmarks[y]
+
+					if landmark_name in self.landmarks_to_render:
+						landmark_data = self.landmarks[hand_name].landmark[y]
+						raw_z = landmark_data.z
+						z = raw_z * (10 ** count_zeros(raw_z))
+
+						hand_data = {
+							landmark_name: {
+								"i": y + 1,
+								"x": scale_to_range(landmark_data.x, [1, 0], [0, 1]),
+								"y": landmark_data.y,
+								"z": z if landmark_name == "wrist" else scale_to_range(z, [0, -1], [0, 1])
+							}
+						}
+						udp.send(address, hand_data)
+
+				# Draw landmarks
+				self.drawing.draw_landmarks(
+					self.image,
+					self.landmarks[hand_name],
+					self.holistic.HAND_CONNECTIONS,
+					self.drawing_styles.get_default_hand_landmarks_style(),
+					self.drawing_styles.get_default_hand_connections_style()
+				)
+			else:
+				udp.send(address, {"isVisible": False})
 
 
 def scale_to_range(value, min, max):
@@ -134,129 +207,50 @@ def count_zeros(decimal):
 	return inf if decimal == 0 else -floor(log10(abs(decimal))) - 1
 
 
+def print_errors(errors):
+	for i in range(len(errors)):
+		print(errors[i])
+	exit(1)
+
+
 if __name__ == "__main__":
 	if len(tf.config.list_physical_devices("GPU")) > 0:
-		udp = UDPCommunicationHandler("127.0.0.1", 7400, 7300)  # ip, client_port, server_port
+		udp = UDPCommunicationHandler("127.0.0.1", 9100, 7300)  # ip, client_port, server_port
 		udp.check_udp_communication("/connect")
 
 		cap = MultiThreadingVideoCapture(0)
 		cap.start()
 
-		mp_drawing = mp.solutions.drawing_utils
-		mp_drawing_styles = mp.solutions.drawing_styles
-		mp_holistic = mp.solutions.holistic
-
-		hand_landmarks = [
-			"wrist", "thumb_cmc", "thumb_mcp", "thumb_ip", "thumb_tip", "index_finger_mcp", "index_finger_pip",
-			"index_finger_dip", "index_finger_tip", "middle_finger_mcp", "middle_finger_pip", "middle_finger_dip",
-			"middle_finger_tip", "ring_finger_mcp", "ring_finger_pip", "ring_finger_dip", "ring_finger_tip",
-			"pinky_mcp", "pinky_pip", "pinky_dip", "pinky_tip"
-		]
-
-		allowed_landmarks = ["wrist", "thumb_tip", "index_finger_tip", "middle_finger_tip", "ring_finger_tip",
-		                    "pinky_tip"]
+		hands = HandLandmarksHandler(mp.solutions, 0.5, 0.5)
 
 		count_frames = 0
 		start = cv.getTickCount()
 
-		with mp_holistic.Holistic(
-				min_detection_confidence=0.5,
-				min_tracking_confidence=0.5
-		) as holistic:
-			while True:
-				if cap.stopped:
-					break
-				else:
-					frame = cap.read()
+		while True:
+			if cap.stopped:
+				break
+			else:
+				frame = cap.read()
 
-				# Get detection results
-				image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-				image.flags.writeable = False
+			# Get detection results
+			hands.run_inference(frame)
+			# Set, send, and display detection results
+			hands.process_inference_data(udp, ["left_hand", "right_hand"])
 
-				results = holistic.process(image)
+			count_frames += 1
 
-				image.flags.writeable = True
-				image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
+			cv.imshow("Real-time keypoint detection", cv.flip(hands.image, 1))
 
-				left_hand = results.left_hand_landmarks
-				right_hand = results.right_hand_landmarks
+			if cv.waitKey(cap.fps_to_ms) == 27:
+				break
+		end = cv.getTickCount()
 
-				# Get coordinates from landmarks
-				if left_hand:
-					udp.send("/left_hand", {"isVisible": True})
+		cap.stop()
 
-					for i in range(len(hand_landmarks)):
-						landmark_name = hand_landmarks[i]
+		elapsed = (end - start) / cv.getTickFrequency()
+		fps = count_frames / elapsed
+		print(f"FPS: {fps:.5f}, Elapsed time: {elapsed:.5f}, Frames processed: {count_frames}")
 
-						if landmark_name in allowed_landmarks:
-							raw_z = left_hand.landmark[i].z
-							z = raw_z * (10 ** count_zeros(raw_z))
-
-							hand_data = {
-								landmark_name: {
-									"i": i + 1,
-									"x": scale_to_range(left_hand.landmark[i].x, [1, 0], [0, 1]),
-									"y": left_hand.landmark[i].y,
-									"z": z if landmark_name == "wrist" else scale_to_range(z, [0, -1], [0, 1])
-								}
-							}
-							udp.send("/left_hand", hand_data)
-				else:
-					udp.send("/left_hand", {"isVisible": False})
-				if right_hand:
-					udp.send("/right_hand", {"isVisible": True})
-
-					for i in range(len(hand_landmarks)):
-						landmark_name = hand_landmarks[i]
-
-						if landmark_name in allowed_landmarks:
-							raw_z = right_hand.landmark[i].z
-							z = raw_z * (10 ** count_zeros(raw_z))
-
-							hand_data = {
-								landmark_name: {
-									"i": i + 1,
-									"x": scale_to_range(right_hand.landmark[i].x, [1, 0], [0, 1]),
-									"y": right_hand.landmark[i].y,
-									"z": z if landmark_name == "wrist" else scale_to_range(z, [0, -1], [0, 1])
-								}
-							}
-							udp.send("/right_hand", hand_data)
-				else:
-					udp.send("/right_hand", {"isVisible": False})
-
-				# Draw landmarks
-				if left_hand:
-					mp_drawing.draw_landmarks(
-						image,
-						left_hand,
-						mp_holistic.HAND_CONNECTIONS,
-						mp_drawing_styles.get_default_hand_landmarks_style(),
-						mp_drawing_styles.get_default_hand_connections_style()
-					)
-				if right_hand:
-					mp_drawing.draw_landmarks(
-						image,
-						right_hand,
-						mp_holistic.HAND_CONNECTIONS,
-						mp_drawing_styles.get_default_hand_landmarks_style(),
-						mp_drawing_styles.get_default_hand_connections_style()
-					)
-
-				count_frames += 1
-
-				cv.imshow("Real-time keypoint detection", cv.flip(image, 1))
-
-				if cv.waitKey(cap.fps_to_ms) == 27:
-					break
-			end = cv.getTickCount()
-
-			cap.stop()
-
-			elapsed = (end - start) / cv.getTickFrequency()
-			fps = count_frames / elapsed
-			print(f"FPS: {fps:.5f}, Elapsed time: {elapsed:.5f}, Frames processed: {count_frames}")
-
-			cv.destroyAllWindows()
+		cv.destroyAllWindows()
 	else:
 		print("MPS GPU is not available")
